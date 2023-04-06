b5fee74a83310954b42e9c30e277b440663cbbf5
thiagonoq <thiago_neves@hotmail.com>
2023-04-05 21:01:35 -0300

    adicionando projeto 7 Days of Code

---
+++ Dia1.js
@@ -0,0 +1,53 @@
+//DIA 1 de 7. Primeiro exercício
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+if (numeroUm == stringUm && numeroUm !== stringUm) { //COMPARAR O numeroUm e a stringUm
+  
+    console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes');
+} else if (numeroUm == stringUm && numeroUm === stringUm) {
+    console.log('As variáveis numeroUm e stringUm possuem mesmo valor e tipo');
+} else {
+  console.log('As variáveis numeroUm e stringUm não tem o mesmo valor');
+}
+
+if (numeroTrinta == stringTrinta && numeroTrinta !== stringTrinta) { //COMPARAR O numeroTrinta e a stringTrinta
+  console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor, mas tipos diferentes');
+} else if (numeroTrinta == stringTrinta && numeroTrinta === stringTrinta){
+  console.log('As variáveis numeroTrinta e stringTrinta possuem mesmo valor e tipo');
+} else {
+  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo');
+}
+
+if (numeroDez == stringDez && numeroDez !== stringDez) { //COMPARAR O numeroDez e a stringDez
+  console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes');
+} else if (numeroDez == stringDez && numeroDez === stringDez){
+  console.log('As variáveis numeroDez e stringDez possuem mesmo valor e tipo');
+} else {
+  console.log('As variáveis numeroDez e stringDez não tem o mesmo valor');
+}
+
+//DIA 2 de 7. Segundo exercício.
+
+let campoNome = document.querySelector('input[type=nome');
+let campoIdade = document.querySelector('input[type=idade');
+let campoLinguagem = document.querySelector('input[type=linguagem');
+let enviar = document.querySelector('input[type=button]');
+
+enviar.onclick = function() {
+  let nome = campoNome.value;
+  let idade = campoIdade.value;
+  let linguagem = campoLinguagem.value;
+
+  alert('Olá ' + nome + ', você tem ' + idade + ' anos e já está aprendendo ' + linguagem + '!');
+  //console.log(nome);
+  //console.log(idade);
+  //console.log(linguagem);
+  campoNome.value = "";
+  campoIdade.value = "";
+  campoLinguagem.value = "";
+}
